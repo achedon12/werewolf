4bd1c06b7562b00afc77c3cbe714a96364734277
Achedon12 <leoderoinleo@gmail.com>
2025-11-06 11:37:54 +0100

    feat: optimization #6

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
+import {createRequire} from 'module';
+
+const require = createRequire(import.meta.url);
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
         optimizeCss: true,
-    }
+    },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
