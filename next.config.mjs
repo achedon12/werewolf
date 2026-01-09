2401ade4b832bfa31580ebe2ba0b970dd44f0e0b
Achedon12 <leoderoinleo@gmail.com>
2026-01-09 08:45:01 +0100

    fix: website config for cache

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,28 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    headers: async () => {
+        return [
+            {
+                source: '/:path((?!_next/static).*)*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=0, must-revalidate'
+                    }
+                ]
+            },
+            {
+                source: '/static/:path*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000, immutable'
+                    }
+                ]
+            }
+        ];
+    },
     experimental: {
         optimizeCss: true,
     },
